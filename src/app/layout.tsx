7a540de24dab108ae0cd06f536872977be09495f
stevebarakat <stevebarakat@gmail.com>
2025-10-30 20:04:28 -0400

    ci

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
+import { Lato } from "next/font/google";
 import { Header } from "@/components/Header";
 import { getMenu, getSiteInfo, getSocialLinks } from "@/lib/wp-data";
 import "./globals.css";
+
+const lato = Lato({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+  variable: "--font-lato",
+});
 
 export const metadata: Metadata = {
   title: "Fort Lauderdale Fishing Trips",
@@ -18,7 +25,7 @@ export default async function RootLayout({
   const socialLinks = await getSocialLinks();
 
   return (
-    <html lang="en">
+    <html lang="en" className={lato.variable}>
       <body>
         <Header menu={menu} siteInfo={siteInfo} socialLinks={socialLinks} />
         <main>{children}</main>
